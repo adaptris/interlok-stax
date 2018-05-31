55d33bf6c4506c17c1771f08019f881be6a64a60
Lewin Chan <lewin.chan@adaptris.com>
2018-05-31 11:30:23 +0100

    Added License.

--- src/main/java/com/adaptris/stax/lms/package-info.java
+++ src/main/java/com/adaptris/stax/lms/package-info.java
@@ -1,4 +1,17 @@
-/**
- * Adds support for large XML processing via STaX within Interlok
- */
+/*
+    Copyright Adaptris Ltd
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
 package com.adaptris.stax.lms;
