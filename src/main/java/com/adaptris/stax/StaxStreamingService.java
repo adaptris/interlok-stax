5026502d15ce44df2bf18a1a3873a46d2674c411
Lewin Chan <lewin.chan@adaptris.com>
2019-01-07 15:07:55 +0000

    INTERLOK-2473 Add StaxStreamingService that has pluggable input/output

---
+++ src/main/java/com/adaptris/stax/StaxStreamingService.java
@@ -0,0 +1,100 @@
+package com.adaptris.stax;
+
+import static com.adaptris.stax.StaxUtils.*;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLEventWriter;
+
+import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.core.CoreException;
+import com.adaptris.core.ServiceException;
+import com.adaptris.core.ServiceImp;
+import com.adaptris.core.util.ExceptionHelper;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+/**
+ * Streaming service that takes using STaX events to stream to and from various XMLEventWriter implementations.
+ * <p>
+ * The most common use case for this would be to stream XML into JSON or vice-versa making use of
+ * {@code com.adaptris.core.json.streaming.JsonStreamingOutputFactory} or {@code com.adaptris.core.json.streaming.JsonStreamingInputFactory} as the
+ * {@link XmlOutputFactoryBuilder} or {@link XmlInputFactoryBuilder} implementations respectively.
+ * </p>
+ * 
+ * @config stax-streaming-service
+ */
+@XStreamAlias("stax-streaming-service")
+public class StaxStreamingService extends ServiceImp {
+
+  private static XmlInputFactoryBuilder defaultInputBuilder = new DefaultInputFactory();
+  private static XmlOutputFactoryBuilder defaultOutputBuilder = new DefaultWriterFactory();
+
+  private XmlInputFactoryBuilder inputBuilder;
+  private XmlOutputFactoryBuilder outputBuilder;
+
+  @Override
+  public void doService(AdaptrisMessage msg) throws ServiceException {
+    XMLEventReader reader = null;
+    XMLEventWriter writer = null;
+    try (InputStream in = msg.getInputStream(); OutputStream out = msg.getOutputStream()) {
+      reader = inputBuilder().build().createXMLEventReader(in);
+      writer = outputBuilder().build().createXMLEventWriter(out);
+      writer.add(reader);
+    }
+    catch (Exception e) {
+      throw ExceptionHelper.wrapServiceException(e);
+    }
+    finally {
+      closeQuietly(reader);
+      closeQuietly(writer);
+    }
+  }
+
+  @Override
+  public void prepare() throws CoreException {
+  }
+
+  @Override
+  protected void initService() throws CoreException {
+  }
+
+  @Override
+  protected void closeService() {
+  }
+
+  public XmlInputFactoryBuilder getInputBuilder() {
+    return inputBuilder;
+  }
+
+  public void setInputBuilder(XmlInputFactoryBuilder b) {
+    this.inputBuilder = b;
+  }
+  
+  public StaxStreamingService withInputBuilder(XmlInputFactoryBuilder b) {
+    setInputBuilder(b);
+    return this;
+  }
+
+  private XmlInputFactoryBuilder inputBuilder() {
+    return getInputBuilder() != null ? getInputBuilder() : defaultInputBuilder;
+  }
+
+  public XmlOutputFactoryBuilder getOutputBuilder() {
+    return outputBuilder;
+  }
+
+  public void setOutputBuilder(XmlOutputFactoryBuilder b) {
+    this.outputBuilder = b;
+  }
+  
+  public StaxStreamingService withOutputBuilder(XmlOutputFactoryBuilder b) {
+    setOutputBuilder(b);
+    return this;
+  }
+  
+  private XmlOutputFactoryBuilder outputBuilder() {
+    return getOutputBuilder() != null ? getOutputBuilder() : defaultOutputBuilder;
+  }
+}
